30a6d8f481b26e1f9ae5f0e8e5e3b04070c2f0f4
Jack <jackmanges@gmail.com>
2024-03-13 12:40:53 -0400

    Added state information recall

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -197,12 +197,25 @@ void GraphicEQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    
+    // Personal Note: This and setStateInformation() below are what enable the plugin parameters to be saved when not opened!
+    
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void GraphicEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid()) {
+        apvts.replaceState(tree);
+        
+        auto chainSettings = getChainSettings(apvts);
+        updatePeakFilters(chainSettings);
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
